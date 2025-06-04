4b5c86afaf944c7012a88acaeca065e1891cc465
Kelvin <kalvin10.kc@gmail.com>
2025-06-04 10:02:51 -0300

    ajustes

--- docker_version/src/aggregate.py
+++ docker_version/src/aggregate.py
@@ -1,7 +1,17 @@
-from pyspark.sql.functions import col, count, avg
+from pyspark.sql.functions import col, count, avg, max, min, when
 
 
-def generate_aggregates(df):
-    return df.groupBy("MOTIVO").agg(
-        count("*").alias("qtde"), avg("DURACAO").alias("media_duracao")
-    )
+def aggregate_to_gold(df_gold):
+    """Agrupa e calcula as métricas diárias"""
+    df_gold = df_gold.groupBy("DT_REF").agg(
+            count("*").alias("QT_CORR"),
+            count(when(col("CATEGORIA") == "Negocio", True)).alias("QT_CORR_NEG"),
+            count(when(col("CATEGORIA") == "Pessoal", True)).alias("QT_CORR_PESS"),
+            max("DISTANCIA").alias("VL_MAX_DIST"),
+            min("DISTANCIA").alias("VL_MIN_DIST"),
+            avg("DISTANCIA").alias("VL_AVG_DIST"),
+            count(when(col("PROPOSITO") == "Reunião", True)).alias("QT_CORR_REUNI"),
+            count(
+                when((col("PROPOSITO").isNotNull()) & (col("PROPOSITO") != "Reunião"), True)
+            ).alias("QT_CORR_NAO_REUNI")
+        )
--- docker_version/src/validate.py
+++ docker_version/src/validate.py
@@ -5,5 +5,5 @@ def validate_schema(df):
     return df.filter(
         col("DATA_INICIO").isNotNull()
         & col("DATA_FIM").isNotNull()
-        & col("MOTIVO").isNotNull()
+        & col("PROPOSITO").isNotNull()
     )
